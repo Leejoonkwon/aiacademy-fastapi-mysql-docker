3f9e4b639e287f79e226960a7598109d9cd090e7
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-08-29 12:17:01 +0900

    2022-08-29

--- app/services/pandas_quiz.py
+++ app/services/pandas_quiz.py
@@ -32,20 +32,37 @@ class PandasQuiz(object):
                                     '1':np.random.randint(10,100,size=3) },
                                orient='index',columns=['0','1','2'])
         ic(df3)  
-    def quiz_4(self) -> None :
-        ic(self.id())
-        ic(self.score())
-        df4 = pd.DataFrame.from_dict({self.id():self.score()},
-                                     orient='index', columns=['국어', '영어', '수학', '사회'])
-        for i in range(8):
-            df4 = pd.concat([df4,pd.DataFrame.from_dict({self.id():self.score()}, 
-                                                        orient='index', columns=['국어', '영어', '수학', '사회'])],axis=0)
-        ic(df4)
+        ''' 
+        
+ Q4 국어, 영어, 수학, 사회 4과목을 시험치른 10명의 학생들의 성적표 작성. 단 점수 0 ~ 100이고 학생은 랜덤 알파벳 5자리 ID 로 표기
+ ic| self.id(): 'HKKHc'
+ic| self.score(): 22
+ic| df4:        국어  영어  수학  사회
+               lDZid  57  90  55  24
+               Rnvtg  12  66  43  11
+               ljfJt  80  33  89  10
+               ZJaje  31  28  37  34
+               OnhcI  15  28  89  19
+               claDN  69  41  66  74    
+               
+               LYawb  65  16  13  20
+               QDBCw  44  32   8  29
+               PZOTP  94  78  79  96
+               GOJKU  62  17  75  49
+'''
     def id(self):
-        rand_str = ''
-        for i in range(5):
-            rand_str += str(random.choice(string.ascii_letters))
-        return rand_str
+        return [ "".join((random.choice(string.ascii_letters) 
+                          for i in range(5))) for i in range(10)]
     
     def score(self):
-        return random.sample(range(0,100), 4)
+        return np.random.randint(0,101,(10,4))
+    
+    def quiz_4(self) :
+        ic(self.id())
+        ic(self.score())
+        df4 = pd.DataFrame(self.score(),
+                           index = self.id(),
+                           columns=['국어', '영어', '수학', '사회'])
+ 
+        ic(df4)
+
